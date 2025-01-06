0738df5b2659f58f8390a3cabac599c2fe24a2ef
Sullivan Delory <sudelory@z2r12p6.42lyon.fr>
2025-01-06 17:59:34 +0100

    first push

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sudelory <sudelory@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 13:19:12 by sudelory          #+#    #+#             */
+/*   Updated: 2025/01/06 17:22:45 by sudelory         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdint.h>
+
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sudelory <sudelory@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 13:19:08 by sudelory          #+#    #+#             */
+/*   Updated: 2025/01/06 17:23:00 by sudelory         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*temp;
+	size_t			i;
+
+	i = 0;
+	if (nmemb == 0 || size == 0)
+		return (0);
+	if ((SIZE_MAX / nmemb) < size)
+		return (NULL);
+	temp = malloc(nmemb * size);
+	if (!temp)
+		return (NULL);
+	while (i < (nmemb * size))
+	{
+		temp[i] = '\0';
+		i++;
+	}
+	return (temp);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*temp;
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	temp = (char *)malloc(i + 1);
+	if (!temp)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		temp[i] = s[i];
+		i++;
+	}
+	temp[i] = '\0';
+	return (temp);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*temp;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	temp = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!temp)
+		return (0);
+	while (s1[i])
+	{
+		temp[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		temp[i] = s2[j];
+		i++;
+		j++;
+	}
+	temp[i] = '\0';
+	return (temp);
+}
